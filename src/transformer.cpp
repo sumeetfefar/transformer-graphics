67e3d1d259d61c63d8a738f6a312093fa5513dee
Ashwin Kachhara <kachhara.ashwin@gmail.com>
2014-09-24 23:14:50 +0530

    drawcube changed to drawcuboid with user defd side lengths. code cleanup

--- src/transformer.cpp
+++ src/transformer.cpp
@@ -4,11 +4,17 @@
 void drawCubeWireframe(){
 	glPolygonMode (GL_FRONT_AND_BACK, GL_LINE);
 	glBegin(GL_QUADS); 
-    // glNormal3d(0, 0, -1);//Back
+    // glNormal3d(0, 0, -1);//Front
     glVertex3f(  0.5, -0.5, 0.5 );
     glVertex3f(  0.5,  0.5, 0.5 );
     glVertex3f( -0.5,  0.5, 0.5 );
     glVertex3f( -0.5, -0.5, 0.5 );
+    
+    //~ glNormal3d(0, 0, -1); // Back
+    glVertex3f( -0.5, -0.5, -0.5 );
+    glVertex3f( -0.5,  0.5, -0.5 );
+    glVertex3f(  0.5,  0.5, -0.5 );
+    glVertex3f(  0.5, -0.5, -0.5 ); 
      
     // Purple side - RIGHT
     glVertex3f( 0.5, -0.5, -0.5 );
@@ -36,47 +42,74 @@ void drawCubeWireframe(){
   glEnd();
 }
 
-void drawCubeSolid(){
+void drawCuboidSolid(float l, float b, float h){
 	glPolygonMode (GL_FRONT_AND_BACK, GL_FILL);
 	glBegin(GL_QUADS); 
     glNormal3d(0, 0, 1);// Front
-    glVertex3f(  0.5, -0.5, 0.5 );
-    glVertex3f(  0.5,  0.5, 0.5 );
-    glVertex3f( -0.5,  0.5, 0.5 );
-    glVertex3f( -0.5, -0.5, 0.5 );
+    glVertex3f(  l/2, -b/2, h/2 );
+    glVertex3f(  l/2,  b/2, h/2 );
+    glVertex3f( -l/2,  b/2, h/2 );
+    glVertex3f( -l/2, -b/2, h/2 );
     
     glNormal3d(0, 0, -1); // Back
-    glVertex3f( -0.5, -0.5, -0.5 );
-    glVertex3f( -0.5,  0.5, -0.5 );
-    glVertex3f(  0.5,  0.5, -0.5 );
-    glVertex3f(  0.5, -0.5, -0.5 );   
+    glVertex3f( -l/2, -b/2, -h/2 );
+    glVertex3f( -l/2,  b/2, -h/2 );
+    glVertex3f(  l/2,  b/2, -h/2 );
+    glVertex3f(  l/2, -b/2, -h/2 );   
     
   
     glNormal3d(1, 0, 0);// RIGHT
-    glVertex3f( 0.5, -0.5, -0.5 );
-    glVertex3f( 0.5,  0.5, -0.5 );
-    glVertex3f( 0.5,  0.5,  0.5 );
-    glVertex3f( 0.5, -0.5,  0.5 );
+    glVertex3f( l/2, -b/2, -h/2 );
+    glVertex3f( l/2,  b/2, -h/2 );
+    glVertex3f( l/2,  b/2,  h/2 );
+    glVertex3f( l/2, -b/2,  h/2 );
   
     glNormal3d(-1, 0, 0);// LEFT
-    glVertex3f( -0.5, -0.5,  0.5 );
-    glVertex3f( -0.5,  0.5,  0.5 );
-    glVertex3f( -0.5,  0.5, -0.5 );
-    glVertex3f( -0.5, -0.5, -0.5 );
+    glVertex3f( -l/2, -b/2,  h/2 );
+    glVertex3f( -l/2,  b/2,  h/2 );
+    glVertex3f( -l/2,  b/2, -h/2 );
+    glVertex3f( -l/2, -b/2, -h/2 );
   
     glNormal3d(0, 1, 0);// TOP
-    glVertex3f(  0.5,  0.5,  0.5 );
-    glVertex3f(  0.5,  0.5, -0.5 );
-    glVertex3f( -0.5,  0.5, -0.5 );
-    glVertex3f( -0.5,  0.5,  0.5 );
+    glVertex3f(  l/2,  b/2,  h/2 );
+    glVertex3f(  l/2,  b/2, -h/2 );
+    glVertex3f( -l/2,  b/2, -h/2 );
+    glVertex3f( -l/2,  b/2,  h/2 );
   
     glNormal3d(0, -1, 0);// BOTTOM
-    glVertex3f(  0.5, -0.5, -0.5 );
-    glVertex3f(  0.5, -0.5,  0.5 );
-    glVertex3f( -0.5, -0.5,  0.5 );
-    glVertex3f( -0.5, -0.5, -0.5 );
+    glVertex3f(  l/2, -b/2, -h/2 );
+    glVertex3f(  l/2, -b/2,  h/2 );
+    glVertex3f( -l/2, -b/2,  h/2 );
+    glVertex3f( -l/2, -b/2, -h/2 );
   glEnd();
 }
+
+//~ void struct_bust(void){
+	//~ glNewList(bust, GL_COMPILE);
+		//~ glColor3f(1.0,1.0,1.0);
+		//~ glScalef(1.8,1.0,1.0);
+		//~ drawCubeSolid();
+		//~ glTranslatef(0.0,-0.75,0.0);
+		//~ glScalef(1.0,1.0,1.0);
+		//~ SetMaterial(mat_specularYELLOW, mat_ambientYELLOW, mat_diffuseYELLOW, mat_shininessYELLOW);
+		//~ glColor3f(1.0,1.0,0.0);
+		//~ glutSolidCube(0.5);
+		//~ //glDisable(GL_LIGHT0);
+		//~ 
+		//~ glTranslatef(0,0.3,0);
+		//~ SetMaterial(mat_specularGRAY, mat_ambientGRAY, mat_diffuseGRAY, mat_shininessGRAY);
+		//~ glColor3ub(128,128,128);
+		//~ glutSolidSphere(0.5,5,5);
+		//~ 
+		//~ /*SetMaterial(mat_specularBLUE, mat_ambientBLUE, mat_diffuseBLUE, mat_shininessBLUE);
+		//~ glColor3ub(0,0,255);
+		//~ glScalef(1.0,0.2,1.0);
+		//~ glutSolidCube(1.0);*/
+		//~ glScalef(1.0,0.2,1.0);
+		
+	//~ glEndList();
+	//~ 
+//~ }
 
 
 void renderGL(void){
@@ -107,15 +140,13 @@ void renderGL(void){
   glRotatef( 35.264, 1.0, 0.0, 0.0);
   glRotatef( -35.0, 0.0, 1.0, 0.0);
 
-  drawCubeSolid();
+  drawCuboidSolid(0.5, 0.5, 0.5);
   glPushMatrix();
 	glTranslatef(1.0, 1.0, 4.0);
-	glRotatef(100.0, 1.0, 0.0, 0.0);
-	drawCubeSolid();
+	glRotatef(150.0, 1.0, 0.0, 0.0);
+	drawCuboidSolid(0.5, 1.0, 0.5);
   glPopMatrix();
 
-  
-  
 }
 
 int main(int argc, char** argv)
