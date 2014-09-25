e3fe091b869a541a5b7fdcafdfb8c52c9f7e490e
Ashwin Kachhara <kachhara.ashwin@gmail.com>
2014-09-25 17:38:58 +0530

    Added display lists for upper & lower arms, thighs, legs. Code cleanup

--- src/transformer.cpp
+++ src/transformer.cpp
@@ -2,8 +2,6 @@
 #include <GL/glu.h>
 #include <GL/gl.h>
 
-
-
 #define head 1
 #define neck 2
 #define torso 3
@@ -20,11 +18,15 @@
 #define right_foot 14
 #define left_foot 15
 
-// float y_angle = -45;
-// float x_angle = 35.264;
+float y_angle = -45;
+float x_angle = 35.264;
 
-float y_angle = 0;
-float x_angle = 0;
+//~ float y_angle = 0;
+//~ float x_angle = 0;
+
+float torso_width, torso_length;
+float upper_arm_length = 0.4, lower_arm_length = 0.5;
+float thigh_length = 0.6, leg_length = 0.6;
 
 void drawCubeWireframe(){
 	glPolygonMode (GL_FRONT_AND_BACK, GL_LINE);
@@ -155,55 +157,111 @@ void struct_torso(void){
 	glEndList();
 }
 
+void struct_right_upper_arm(void){
+	glNewList(right_upper_arm, GL_COMPILE);
+		drawCuboidSolid(0.15, upper_arm_length, 0.15);
+	glEndList();
+}
+
+void struct_right_lower_arm(void){
+	glNewList(right_lower_arm, GL_COMPILE);
+		drawCuboidSolid(0.11, lower_arm_length, 0.11);
+	glEndList();
+}
+
+void struct_left_upper_arm(void){
+	glNewList(left_upper_arm, GL_COMPILE);
+		drawCuboidSolid(0.15, upper_arm_length, 0.15);
+	glEndList();
+}
+
+void struct_left_lower_arm(void){
+	glNewList(left_lower_arm, GL_COMPILE);
+		drawCuboidSolid(0.11, lower_arm_length, 0.11);
+	glEndList();
+}
+
+void struct_right_thigh(void){
+	glNewList(right_thigh, GL_COMPILE);
+		drawCuboidSolid(0.15, thigh_length, 0.15);
+	glEndList();
+}
+
+void struct_right_leg(void){
+	glNewList(right_leg, GL_COMPILE);
+		drawCuboidSolid(0.12, leg_length, 0.12);
+	glEndList();
+}
+
 void init_structures(){
 	struct_head();
 	struct_neck();
 	struct_torso();
+	struct_right_upper_arm();
+	struct_right_lower_arm();
+	struct_left_upper_arm();
+	struct_left_lower_arm();
+	struct_right_thigh();
+	struct_right_leg();
+}
+
+void initGL(void){
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	glEnable(GL_CULL_FACE); 
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_COLOR_MATERIAL);
+	glEnable(GL_AUTO_NORMAL);
+	glShadeModel(GL_SMOOTH);
+
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CCW);
+
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+	glEnable (GL_LIGHT0);
+
+	GLfloat global_ambient[] = { 0.4, 0.4, 0.4, 1 };
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_ambient);
+	GLfloat diffuse[] = { 0.5f, 0.5f, 1, 1 };
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+	GLfloat light_position[] = { 1.0f, 0.5f, 0, 0 };
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+
+
+	glMatrixMode(GL_MODELVIEW);
+	gluPerspective(120,1,0.1,10);
+	gluLookAt(0,0,1, 0, 0, 0, 0, 1,0);
 }
 
 void renderGL(void){
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glLoadIdentity();
-  glEnable(GL_CULL_FACE); 
-
-  glEnable(GL_LIGHTING);
-  glEnable(GL_COLOR_MATERIAL);
-  glEnable(GL_AUTO_NORMAL);
-  glShadeModel(GL_SMOOTH);
   
-  glCullFace(GL_BACK);
-  glFrontFace(GL_CCW);
+	initGL();
+	//~ glTranslatef(0, 0, -1);
 
-  glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-  glEnable (GL_LIGHT0);
+	glRotatef( x_angle, 1.0, 0.0, 0.0);
+	glRotatef( y_angle, 0.0, 1.0, 0.0);
 
-  GLfloat global_ambient[] = { 0.4, 0.4, 0.4, 1 };
-  glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_ambient);
-  GLfloat diffuse[] = { 0.5f, 0.5f, 1, 1 };
-  glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
-  GLfloat light_position[] = { 1.0f, 0.5f, 0, 0 };
-  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-
-
-  glMatrixMode(GL_MODELVIEW);
-  gluPerspective(120,1,0.1,10);
-  gluLookAt(0,0,1, 0, 0, 0, 0, 1,0);
-  glTranslatef(0, 0, -1);
-  
-  glRotatef( x_angle, 1.0, 0.0, 0.0);
-  glRotatef( y_angle, 0.0, 1.0, 0.0);
-
-  //~ drawCuboidSolid(0.5, 0.5, 0.5);
-  //~ glPushMatrix();
+	//~ drawCuboidSolid(0.5, 0.5, 0.5);
+	//~ glPushMatrix();
 	//~ glTranslatef(1.0, 1.0, 4.0);
 	//~ glRotatef(150.0, 1.0, 0.0, 0.0);
 	//~ drawCuboidSolid(0.5, 1.0, 0.5);
-  //~ glPopMatrix();
-  //~ glRotatef(30, 0, 1, 0);
-  
-  glCallList(torso);
+	//~ glPopMatrix();
+	//~ glRotatef(30, 0, 1, 0);
 
+	glCallList(right_upper_arm);
+	glPushMatrix();
+		glTranslatef(0, -(upper_arm_length+lower_arm_length)/2, 0);
+		glCallList(right_lower_arm);
+	glPopMatrix();
+	
+	//~ glCallList(right_thigh);
+	//~ glPushMatrix();
+		//~ glTranslatef(0, -(thigh_length+leg_length)/2, 0);
+		//~ glCallList(right_leg);
+	//~ glPopMatrix();
 }
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
@@ -211,78 +269,78 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GL_TRUE);
 	else if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
-		y_angle -= 10;
-	else if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
 		y_angle += 10;
+	else if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+		y_angle -= 10;
 	else if (key == GLFW_KEY_UP && action == GLFW_PRESS)
 		x_angle -= 10;
 	else if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
-	x_angle += 10;
+		x_angle += 10;
 }
 
 int main(int argc, char** argv)
 {
-  //! The pointer to the GLFW window
-  GLFWwindow* window;
+	//! The pointer to the GLFW window
+	GLFWwindow* window;
 
-  //! Setting up the GLFW Error callback
-  glfwSetErrorCallback(csX75::error_callback);
+	//! Setting up the GLFW Error callback
+	glfwSetErrorCallback(csX75::error_callback);
 
-  //! Initialize GLFW
-  if (!glfwInit())
-    return -1;
+	//! Initialize GLFW
+	if (!glfwInit())
+	return -1;
 
-  //! Create a windowed mode window and its OpenGL context
-  window = glfwCreateWindow(800, 800, "CS475/CS675 OpenGL Framework", NULL, NULL);
-  if (!window)
-    {
-      glfwTerminate();
-      return -1;
-    }
-  
-  //! Make the window's context current 
-  glfwMakeContextCurrent(window);
+	//! Create a windowed mode window and its OpenGL context
+	window = glfwCreateWindow(800, 800, "CS475/CS675 OpenGL Framework", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-  //Initialize GLEW
-  GLenum err = glewInit();
-  if (GLEW_OK != err)
-    {
-      //Problem: glewInit failed, something is seriously wrong.
-      std::cerr<<"GLEW Init Failed : %s"<<std::endl;
-    }
+	//! Make the window's context current 
+	glfwMakeContextCurrent(window);
 
-  //Print and see what context got enabled
-  std::cout<<"Vendor: "<<glGetString (GL_VENDOR)<<std::endl;
-  std::cout<<"Renderer: "<<glGetString (GL_RENDERER)<<std::endl;
-  std::cout<<"Version: "<<glGetString (GL_VERSION)<<std::endl;
-  std::cout<<"GLSL Version: "<<glGetString (GL_SHADING_LANGUAGE_VERSION)<<std::endl;
+	//Initialize GLEW
+	GLenum err = glewInit();
+	if (GLEW_OK != err)
+	{
+		//Problem: glewInit failed, something is seriously wrong.
+		std::cerr<<"GLEW Init Failed : %s"<<std::endl;
+	}
 
-  //Keyboard Callback
-  glfwSetKeyCallback(window, key_callback);
-  //Framebuffer resize callback
-  glfwSetFramebufferSizeCallback(window, csX75::framebuffer_size_callback);
+	//Print and see what context got enabled
+	std::cout<<"Vendor: "<<glGetString (GL_VENDOR)<<std::endl;
+	std::cout<<"Renderer: "<<glGetString (GL_RENDERER)<<std::endl;
+	std::cout<<"Version: "<<glGetString (GL_VERSION)<<std::endl;
+	std::cout<<"GLSL Version: "<<glGetString (GL_SHADING_LANGUAGE_VERSION)<<std::endl;
 
-  // Ensure we can capture the escape key being pressed below
-  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	//Keyboard Callback
+	glfwSetKeyCallback(window, key_callback);
+	//Framebuffer resize callback
+	glfwSetFramebufferSizeCallback(window, csX75::framebuffer_size_callback);
 
-  //Initialize GL state
-  csX75::initGL();
-  init_structures();
-  // Loop until the user closes the window
-  while (glfwWindowShouldClose(window) == 0)
-    {
-       
-      // Render here
-      renderGL();
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-      // Swap front and back buffers
-      glfwSwapBuffers(window);
-      
-      // Poll for and process events
-      glfwPollEvents();
-    }
-  
-  glfwTerminate();
-  return 0;
+	//Initialize GL state
+	csX75::initGL();
+	init_structures();
+	// Loop until the user closes the window
+	while (glfwWindowShouldClose(window) == 0)
+	{
+
+		// Render here
+		renderGL();
+
+		// Swap front and back buffers
+		glfwSwapBuffers(window);
+
+		// Poll for and process events
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
 
